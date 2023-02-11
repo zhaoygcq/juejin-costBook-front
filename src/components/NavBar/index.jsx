91cb737418e7b80925106d40977354cabbdd1a6b
zhaoyinggang <1060401583@qq.com>
2023-02-11 17:13:03 +0800

    fix: 根据路由决定底部的navBar的选中状态

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { TabBar } from "zarm";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import CustomIcon from "../CustomIcon";
 import s from "./style.module.less";
 
 const NavBar = ({ showNav }) => {
-  const [activeKey, setActiveKey] = useState("/");
+  const location = useLocation(); // 拿到 location 实例
+  const { pathname } = location; // 获取当前路径
+  const [activeKey, setActiveKey] = useState(pathname);
   const navigateTo = useNavigate();
 
   const changeTab = (path) => {
